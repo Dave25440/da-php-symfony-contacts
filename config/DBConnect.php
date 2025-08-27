b3c2ad529b7223ce92d46c3d0688ba6b43812f9b
David Horès <hores.d@gmail.com>
2025-08-27 16:50:07 +0200

    Créez vos première classes : Créez la classe DBConnect

---
+++ config/DBConnect.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Config;
+
+use PDO;
+use PDOException;
+use Exception;
+
+class DBConnect
+{
+    private ?PDO $pdo = null;
+
+    public function __construct(
+        private string $host = 'localhost',
+        private string $name = 'contacts',
+        private int $port = 3306,
+        private string $user = 'root',
+        private string $password = ''
+    ) {}
+
+    public function getPDO(): PDO
+    {
+        if ($this->pdo === null) {
+            try {
+                $this->pdo = new PDO(
+                    'mysql:host='.$this->host.';dbname='.$this->name.';port='.$this->port.';charset=utf8',
+                    $this->user,
+                    $this->password,
+                    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+                );
+            } catch (PDOException $e) {
+                error_log('Erreur de connexion PDO : ' . $e->getMessage());
+                throw new Exception('Impossible de se connecter à la base de données.');
+            }
+        }
+
+        return $this->pdo;
+    }
+}
--- main.php
+++ main.php
@@ -1,11 +1,26 @@
 <?php
 
-    while (true) {
-        $line = readline("Entrez votre commande : ");
-        echo "Vous avez saisi : $line\n";
+spl_autoload_register(static function ($fqcn): void {
+    $path = sprintf('%s.php', str_replace(['App\\Config', '\\'], ['config', '/'], $fqcn));
+    require_once $path;
+});
 
-        switch ($line) {
-            case "list":
-                echo "Affichage de la liste\n";
-        }
+use App\Config\DBConnect;
+
+$db = new DBConnect();
+$pdo = $db->getPDO();
+
+var_dump($pdo);
+
+while (true) {
+    $line = readline("Entrez votre commande : ");
+    echo "Vous avez saisi : $line\n";
+
+    switch ($line) {
+        case 'list':
+            echo "Affichage de la liste\n";
+            break;
+        default:
+            echo "Commande inconnue\n";
     }
+}

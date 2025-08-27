2dadcdcf4df5b5cb5e9f91bb883d74ef3a05eee5
David Horès <hores.d@gmail.com>
2025-08-27 10:46:41 +0200

    Créez la commande list

--- main.php
+++ main.php
@@ -3,4 +3,9 @@
     while (true) {
         $line = readline("Entrez votre commande : ");
         echo "Vous avez saisi : $line\n";
+
+        switch ($line) {
+            case "list":
+                echo "Affichage de la liste\n";
+        }
     }
